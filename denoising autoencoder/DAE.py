cb048afebceed404d4b0911f6f7d3b15a9dd8c36
woshiljw <3222413873@qq.com>
2018-11-30 18:01:19 +0800

    add batch normalization

--- denoising autoencoder/DAE.py
+++ denoising autoencoder/DAE.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import matplotlib.pyplot as plt
 from Noise import Noise
+from tensorflow.python.layers import normalization
 
 def init_weights(shape):
     '''
@@ -90,27 +91,35 @@ def mkModel(x):
     x_image = tf.reshape(x,[-1,28,28,1])
 
     #encode
+
     conv1 = conv_layer(x_image,filters["conv1/deconv1"],name='conv1')
     conv1_pool = max_pool_2x2(conv1,name='conv1_pool')
+    conv1_pool = normalization.batch_normalization(conv1_pool)
 
     conv2 = conv_layer(conv1_pool,filters["conv2/deconv2"],name='conv2')
     conv2_pool = max_pool_2x2(conv2,name='conv2_pool')
+    conv2_pool = normalization.batch_normalization(conv2_pool)
 
     encode = full_layer(tf.reshape(conv2_pool,[-1,7*7*8]),20)
+    encode = normalization.batch_normalization(encode)
 
     decode = full_layer(encode,7*7*8)
+    decode = normalization.batch_normalization(decode)
+
 
     unpool1 = unpooling(tf.reshape(decode,[-1,7,7,8]),[2,2,8,8],tf.shape(conv2),name='unpool1')
     print("unpool:{}".format(unpool1.shape))
     deconv1 = tf.nn.relu(deconv_layer(unpool1,filters["conv2/deconv2"],tf.shape(conv1_pool),name='deconv1'))
+    deconv1 = normalization.batch_normalization(deconv1)
+
     print("deconv1:{}".format(deconv1.shape))
 
     unpool2 = unpooling(deconv1,[2,2,16,16],tf.shape(conv1),name='unpool2')
     print("unpool2:{}".format(unpool2.shape))
-    decode = tf.nn.sigmoid(deconv_layer(unpool2,filters["conv1/deconv1"],tf.shape(x_image),name='decode2'))
+    decoder = tf.nn.relu(deconv_layer(unpool2,filters["conv1/deconv1"],tf.shape(x_image),name='decode2'))
     print("decode2:{}".format(decode.shape))
 
-    return x_image,decode
+    return x_image,decoder
 
 x_image,decode=mkModel(x)
 
@@ -126,8 +135,8 @@ with tf.Session() as sess:
         x_,y = mnist.train.next_batch(64)
         x_noise = Noise(x_)
         if i % 10 == 0:
-            print('In {} step,cost: {}'.format(i,sess.run(cost,feed_dict={x:x_noise.GaussianNoise(0.1),y_i:x_})))
-        sess.run(train_step,feed_dict={x:x_noise.GaussianNoise(0.1),y_i:x_})
+            print('In {} step,cost: {}'.format(i,sess.run(cost,feed_dict={x:x_noise.GaussianNoise(0),y_i:x_})))
+        sess.run(train_step,feed_dict={x:x_noise.GaussianNoise(0),y_i:x_})
 
     x_test = mnist.test.images.reshape(10, 1000, 784)
 
